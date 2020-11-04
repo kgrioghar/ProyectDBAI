ea846e47212c2cc0049ab8099ef4e1cd5d39e869
yperez28 <48894032+yperez28@users.noreply.github.com>
2020-11-03 20:49:40 -0600

    Create sequences for id

---
+++ sequences.sql
@@ -0,0 +1,269 @@
+-- Sequence for the id_administrator of the administrator's table
+create sequence s_administrator
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_brand of the brand's table
+create sequence s_brand
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+
+-- Sequence for the id_canton of the canton's table
+create sequence s_canton
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_card of the card's table
+create sequence s_card
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_category of the category's table
+create sequence s_category
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_clock of the clock's table
+create sequence s_clock
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_commentary of the commentary's table
+create sequence s_commentary
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_country of the country's table
+create sequence s_country
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_user of the db_user's table
+create sequence s_user
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_district of the district's table
+create sequence s_district
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_email of the email's table
+create sequence s_email
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_favorite of the favorite_clock_list table
+create sequence s_favorite
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_identification of the identification's table
+create sequence s_identification
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_message of the message's table
+create sequence s_message
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_nationality of the nationality's table
+create sequence s_nationality
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_parameter of the parameter's table
+create sequence s_parameter
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_payment_method of the payment_method's table
+create sequence s_payment_method
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_person of the person's table
+create sequence s_person
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_phone of the phone's table
+create sequence s_phone
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_photo of the photo's table
+create sequence s_photo
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_province of the province's table
+create sequence s_province
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_purchase of the purchase's table
+create sequence s_purchase
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_record of the purchase_record table
+create sequence s_purchase_record
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_sale of the sale's table
+create sequence s_sale
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_sale_record of the sale_record table
+create sequence s_sale_record
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_score of the score's table
+create sequence s_score
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_shipping of the shipping_type table
+create sequence s_shipping_type
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_shoppig of the shopping_card's table
+create sequence s_shopping_card
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_status of the status table
+create sequence s_status
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
+    
+-- Sequence for the id_vintage of the vintage_classification table
+create sequence s_vintage
+    start with 0
+    increment by 1
+    minvalue 0
+    maxvalue 1000000000
+    nocache
+    nocycle;
